a6340bae64f411dc3ec6422307e13747cd8d49e9
zhouqiuhong <2358636483@qq.com>
2018-08-08 12:42:23 +0800

    update3

--- apps/course/models.py
+++ apps/course/models.py
@@ -2,15 +2,17 @@
 from datetime import datetime
 
 from django.db import models
+from organization.models import CourseOrganization
 
 # Create your models here.
 
 
 class Course(models.Model):
+    course_org = models.ForeignKey(CourseOrganization, verbose_name=u"课程机构", null=True, blank=True)
     name = models.CharField(max_length=100,verbose_name=u"课程名")
     desc = models.CharField(max_length=300,verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u"课程详情")
-    degree = models.CharField(choices=(('chuji', u'初级'), ('zhongji', u'中级'), ('gaoji', u'高级')), max_length=2)
+    degree = models.CharField(choices=(('chuji', u'初级'), ('zhongji', u'中级'), ('gaoji', u'高级')), max_length=10)
     learn_time = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
     students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_num = models.IntegerField(default=0,verbose_name=u"收藏人数")
@@ -27,7 +29,7 @@ class Course(models.Model):
 
 
 class Lesson(models.Model):
-    course = models.ForeignKey(Course, verbose_name=u"课程")
+    course = models.ForeignKey(Course, verbose_name=u"课程",null=True, blank=True)
     name = models.CharField(max_length=100, verbose_name=u"章节名")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
--- apps/organization/forms.py
+++ apps/organization/forms.py
@@ -4,6 +4,8 @@
 @file:forms.py
 @time:2018/8/7 000717:12
 """
+
+import re
 from django import forms
 
 from operation.models import UserAsk
@@ -13,3 +15,12 @@ class UserAskForm(forms.ModelForm):
     class Meta:
         model = UserAsk
         fields = ['name', 'course_name', 'mobile']
+
+    def clean_mobile(self):
+        mobile = self.cleaned_data['mobile']
+        REXGN = "^((1[3,5,8][0-9])|(14[5,7])|(17[0,6,7,8])|(19[7]))\\d{8}$"
+        p = re.compile(REXGN)
+        if p.match(mobile):
+            return mobile
+        else:
+            raise forms.ValidationError(u"手机号码非法", code="mobile_invalid")
